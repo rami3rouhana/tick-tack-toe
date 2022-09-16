0ce6d47399f2dc1085421ce7d5c1d1574b3648d5
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-16 20:37:50 +0300

    add repetition resitriction

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Add game setup
-let gameSetup = [["red","yellow","red"],["red","yellow","red"],["red","yellow","red"]];
+let gameSetup = [[0,0,0],[0,0,0],[0,0,0]];
 
 // Add players
-const players = ["red","yellow"];
+let players = ["red","yellow"];
 
 // Add players turn
 let playerTurn = players[Math.round(Math.random()*1)];
@@ -12,6 +12,9 @@ const input = []
 
 // Add player input
 gameSetup[input[0],input[1]] = players[playerTurn];
+
+// Add player Setup
+playerSetup = [[0,0,0],[0,0,0],[0,0,0]];
 
 // Game Score
 let yellowScore = 0;
@@ -62,32 +65,84 @@ const gameRules = [
   ]
 
 // Convert table
+const convertTable = () => {
+    playerSetup = [[0,0,0],[0,0,0],[0,0,0]];
 
-playerSetup = [[0,0,0],[0,0,0],[0,0,0]]
-
-for(let j=0; j<gameSetup.length; j++){
-    for(let i=0; i<gameSetup.length; i++){
-        if(gameSetup[j][i] == playerTurn){
-            playerSetup[j][i] = 1;
+    for(let j=0; j<gameSetup.length; j++){
+        for(let i=0; i<gameSetup.length; i++){
+            if(gameSetup[j][i] == playerTurn){
+                playerSetup[j][i] = 1;
+            }
         }
     }
 }
 
 // Add win game
-gameRules.forEach(gameRule => {
-    let winCombo = 0;
-    for(let i = 0; i<3; i++){
-        if(JSON.stringify(gameRule[i]) === JSON.stringify(playerSetup[i])){
-            winCombo++;
+const checkWin = () => {    
+    convertTable();
+    gameRules.forEach(gameRule => {
+        let winCombo = 0;
+        for(let i = 0; i<3; i++){
+            if(JSON.stringify(gameRule[i]) === JSON.stringify(playerSetup[i])){
+                winCombo++;
+            }
         }
-    }
-    if(winCombo === 3) {
-        if(playerTurn === 'yellow'){
-            yellowScore += 1;
+        if(winCombo === 3) {
+            if(playerTurn === 'yellow'){
+                yellowScore += 1;
+            }
+            else{
+                redScore += 1;
+            }
+            alert(`${playerTurn} Wins!`);
         }
-        else{
-            redScore += 1;
-        }
-        alert(`${playerTurn} Wins!`);
-    }
-});
+        players.indexOf(playerTurn)==0?playerTurn="yellow":playerTurn="red";
+    });
+}
+
+// Event listners
+document.getElementById("top-left").addEventListener("click",() => {
+    if(gameSetup[0][0]==0){
+        gameSetup[0][0]=playerTurn;
+        checkWin();}
+})
+document.getElementById("top-center").addEventListener("click",() => {
+    if(gameSetup[0][1]==0){
+        gameSetup[0][1]=playerTurn;
+        checkWin();}
+})
+document.getElementById("top-right").addEventListener("click",() => {
+    if(gameSetup[0][2]==0){
+        gameSetup[0][2]=playerTurn;
+        checkWin();}
+})
+document.getElementById("middle-left").addEventListener("click",() => {
+    if(gameSetup[1][0]==0){
+        gameSetup[1][0]=playerTurn;
+        checkWin();}
+})
+document.getElementById("middle-center").addEventListener("click",() => {
+    if(gameSetup[1][1]==0){
+        gameSetup[1][1]=playerTurn;
+        checkWin();}
+})
+document.getElementById("middle-right").addEventListener("click",() => {
+    if(gameSetup[1][2]==0){
+        gameSetup[1][2]=playerTurn;
+        checkWin();}
+})
+document.getElementById("bottom-left").addEventListener("click",() => {
+    if(gameSetup[2][0]==0){
+        gameSetup[2][0]=playerTurn;
+        checkWin();}
+})
+document.getElementById("bottom-center").addEventListener("click",() => {
+    if(gameSetup[2][1]==0){
+        gameSetup[2][1]=playerTurn;
+        checkWin();}
+})
+document.getElementById("bottom-right").addEventListener("click",() => {
+    if(gameSetup[2][2]==0){
+        gameSetup[2][2]=playerTurn;
+        checkWin();}
+})
