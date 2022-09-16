a1556258b94bdc11040bdbe31a1b9324f92ed77c
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-16 17:55:42 +0300

    add game rules

--- index.js
+++ index.js
@@ -6,3 +6,53 @@ const players = ["red","yellow"];
 
 // Add players turn
 let playerTurn = players[Math.round(Math.random()*1)];
+
+// Get player input
+const input = []
+
+// Add player input
+gameSetup[input[0],input[1]] = players[playerTurn];
+
+// Game rules
+const gameRules = [
+    [
+      [1,0,0],
+      [1,0,0],
+      [1,0,0]
+    ],
+    [
+      [0,1,0],
+      [0,1,0],
+      [0,1,0]
+    ],
+    [
+      [0,0,1],
+      [0,0,1],
+      [0,0,1]
+    ],
+    [
+      [1,1,1],
+      [0,0,0],
+      [0,0,0]
+    ],
+    [
+      [0,0,0],
+      [1,1,1],
+      [0,0,0]
+    ],
+    [
+      [0,0,0],
+      [0,0,0],
+      [1,1,1]
+    ],
+    [
+      [0,0,1],
+      [0,1,0],
+      [1,0,0]
+    ],
+    [
+      [1,0,0],
+      [0,1,0],
+      [0,0,1]
+    ]
+  ]
