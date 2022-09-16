9ad2c4a2065bdccc03eab2edce1f2fd4aafc4757
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-16 19:53:37 +0300

    add scoring system

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const input = []
 // Add player input
 gameSetup[input[0],input[1]] = players[playerTurn];
 
+// Game Score
+let yellowScore = 0;
+let redScore = 0;
+
 // Game rules
 const gameRules = [
     [
@@ -69,3 +73,21 @@ for(let j=0; j<gameSetup.length; j++){
     }
 }
 
+// Add win game
+gameRules.forEach(gameRule => {
+    let winCombo = 0;
+    for(let i = 0; i<3; i++){
+        if(JSON.stringify(gameRule[i]) === JSON.stringify(playerSetup[i])){
+            winCombo++;
+        }
+    }
+    if(winCombo === 3) {
+        if(playerTurn === 'yellow'){
+            yellowScore += 1;
+        }
+        else{
+            redScore += 1;
+        }
+        alert(`${playerTurn} Wins!`);
+    }
+});
